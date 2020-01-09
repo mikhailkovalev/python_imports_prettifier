a9aec903d5e6510c73f5e2ee73495ba49d9cdc2e
Михаил Ковалев <kovalev.me@obt-vlg.ru>
2020-01-09 15:43:24 +0400

    Initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from pprint import (
+    pprint as pp,
+)
+from typing import (
+    List,
+)
+
+
+def main(argv: List[str]) -> None:
+    """
+    Принимает на вход список путей к py-файлам, у
+    которых нужно отформатировать импорты.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
